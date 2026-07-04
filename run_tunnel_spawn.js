47ba1020b2eebe7714adb4953b35e71f32e4aef0
sachin-paranjape <sachinparanjape17@gmail.com>
2026-07-04 15:46:34 +0530

    Updated for Zapbox

--- run_tunnel_spawn.js
+++ run_tunnel_spawn.js
@@ -7,16 +7,22 @@ const logPath = path.join(__dirname, 'tunnel_url.txt');
 console.log('Spawning npx.cmd tmole 8000...');
 const child = spawn('npx.cmd', ['tmole', '8000'], { shell: true });
 
+let urlFound = false;
+
 child.stdout.on('data', (data) => {
   const output = data.toString();
   console.log('STDOUT:', output);
   
-  // Look for any https:// URL in the output
-  const match = output.match(/https:\/\/[^\s]+/);
-  if (match) {
-    const url = match[0];
-    console.log('Found tunnel URL:', url);
-    fs.writeFileSync(logPath, url, 'utf8');
+  // Only capture the first tunnelmole URL, ignore all subsequent https:// matches
+  // (e.g. Slack's unfurler bot User-Agent contains https://api.slack.com/robots)
+  if (!urlFound) {
+    const match = output.match(/https:\/\/[^\s]+/);
+    if (match) {
+      const url = match[0];
+      console.log('Found tunnel URL:', url);
+      fs.writeFileSync(logPath, url, 'utf8');
+      urlFound = true;
+    }
   }
 });
 
